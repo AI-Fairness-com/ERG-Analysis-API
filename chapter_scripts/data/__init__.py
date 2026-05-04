4943a625a2e88487dc7ee658bbea01192b4985a0
Hamid Tavakoli <info@ai-fairness.com>
2026-05-04 13:18:58 +0100

    Create __init__.py for data package
    Add docstring for ERG Analysis API data package.

---
+++ chapter_scripts/data/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+ERG Analysis API - Data Package
+
+This package contains de-identified sample ERG recordings and
+normative reference data.
+
+All data is fully anonymised and complies with GDPR Article 5.
