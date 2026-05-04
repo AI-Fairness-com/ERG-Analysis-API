51dff668f236de5468ed093dff62e5ac3c62177e
Hamid Tavakoli <info@ai-fairness.com>
2026-05-04 14:07:54 +0100

    Delete chapter_scripts/data directory

--- chapter_scripts/data/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-ERG Analysis API - Data Package
-
-This package contains de-identified sample ERG recordings and
-normative reference data.
-
-All data is fully anonymised and complies with GDPR Article 5.
